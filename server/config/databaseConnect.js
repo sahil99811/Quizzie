3e53cb2c1c9bf602b1c9702fe1f347ee81fef214
sahil99811 <sp220253@gmail.com>
2024-05-28 15:05:15 +0530

    fix database connect error

--- server/config/databaseConnect.js
+++ server/config/databaseConnect.js
@@ -1,8 +1,6 @@
 const mongoose=require('mongoose');
 exports.dbConnect= () => {
    return mongoose.connect(process.env.MONGODB_URL);
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 068517f076f7041b2f48132a8fc4047abcff4004
+
+
