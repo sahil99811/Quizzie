2e3d33191fa567381c4fc0eb43b0afb5287840a8
sahil99811 <sp220253@gmail.com>
2024-06-03 19:13:43 +0530

    fix orgin issue

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 // Optionally, configure CORS options
 app.use(cors({
-  origin: '*', // Allow requests from any origin
+  origin: `${process.env.FRONTEND_BASE_URL}`, // Allow requests from any origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
   
 }));
