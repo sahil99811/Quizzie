c9473aa31e287c8a99ce598f1c72281703a15b11
Sahil Patel <107112043+sahil99811@users.noreply.github.com>
2024-10-26 09:29:41 +0530

    Update databaseConnect.js

--- server/config/databaseConnect.js
+++ server/config/databaseConnect.js
@@ -1,6 +1,9 @@
 const mongoose=require('mongoose');
-exports.dbConnect= () => {
-   return mongoose.connect(process.env.MONGODB_URL);
-};
+// exports.dbConnect= () => {
+//    return mongoose.connect(process.env.MONGODB_URL);
+// };
 
+exports.dbConnect= () => {
+   return mongoose.connect(process.env.DATABASE_URL);
+};
 
